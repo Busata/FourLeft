c7c0a65f410111d323d17161d2dd009ac672268c
Dries De Smet <driesdesmet@gmail.com>
2022-11-02 11:39:50 +0100

    Add stage condition

--- api/application/src/main/java/io/busata/fourleft/endpoints/club/automated/service/ClubChampionshipCopier.java
+++ api/application/src/main/java/io/busata/fourleft/endpoints/club/automated/service/ClubChampionshipCopier.java
@@ -1,6 +1,7 @@
 package io.busata.fourleft.endpoints.club.automated.service;
 
 import io.busata.fourleft.domain.options.models.CountryOption;
+import io.busata.fourleft.domain.options.models.StageConditionOption;
 import io.busata.fourleft.domain.options.models.StageOption;
 import io.busata.fourleft.domain.options.models.VehicleClass;
 import io.busata.fourleft.gateway.racenet.RacenetGateway;
@@ -11,6 +12,10 @@ import io.busata.fourleft.importer.ClubSyncService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Random;
+
 import static io.busata.fourleft.gateway.racenet.dto.club.championship.creation.DR2ChampionshipCreateBuilder.championship;
 import static io.busata.fourleft.gateway.racenet.dto.club.championship.creation.DR2ChampionshipCreateEventBuilder.event;
 import static io.busata.fourleft.gateway.racenet.dto.club.championship.creation.DR2ChampionshipCreateStageBuilder.stage;
@@ -18,6 +23,7 @@ import static io.busata.fourleft.gateway.racenet.dto.club.championship.creation.
 @Component
 @RequiredArgsConstructor
 public class ClubChampionshipCopier {
+    private Random random = new Random(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
     private final ClubSyncService clubSyncService;
     private final RacenetGateway client;
 
@@ -32,10 +38,14 @@ public class ClubChampionshipCopier {
                         .durationMins(60)
                         .vehicle(VehicleClass.findById(fromEvent.getVehicleClass()))
                         .withStages(
-                                fromEvent.getStages().stream().map(fromStage ->
-                                        stage()
-                                                .degradation(SurfaceDegradation.MEDIUM)
-                                                .route(StageOption.findByName(fromStage.getName()))).toArray(DR2ChampionshipCreateStageBuilder[]::new)
+                                fromEvent.getStages().stream().map(fromStage -> {
+                                    final var dryConditions = CountryOption.findById(fromEvent.getCountry()).getDryConditions();
+
+                                    return stage()
+                                            .stageConditionOption(dryConditions.get(random.nextInt(dryConditions.size())))
+                                            .degradation(SurfaceDegradation.MEDIUM)
+                                            .route(StageOption.findByName(fromStage.getName()));
+                                }).toArray(DR2ChampionshipCreateStageBuilder[]::new)
                         )).toArray(DR2ChampionshipCreateEventBuilder[]::new)).build();
 
 
