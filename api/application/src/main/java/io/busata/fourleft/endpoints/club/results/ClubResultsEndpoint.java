27a13a464ff92d5548bcd7b9d18612eae868c846
Dries De Smet <driesdesmet@gmail.com>
2023-01-15 19:43:19 +0100

    send event after manual refresh

--- api/application/src/main/java/io/busata/fourleft/endpoints/club/results/ClubResultsEndpoint.java
+++ api/application/src/main/java/io/busata/fourleft/endpoints/club/results/ClubResultsEndpoint.java
@@ -1,6 +1,7 @@
 package io.busata.fourleft.endpoints.club.results;
 
 import io.busata.fourleft.api.Routes;
+import io.busata.fourleft.api.messages.LeaderboardUpdated;
 import io.busata.fourleft.api.models.ChampionshipEventSummaryTo;
 import io.busata.fourleft.api.models.ChampionshipStageSummaryTo;
 import io.busata.fourleft.api.models.ChampionshipStandingEntryTo;
@@ -14,6 +15,7 @@ import io.busata.fourleft.domain.clubs.models.Club;
 import io.busata.fourleft.importer.updaters.RacenetSyncService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,11 +34,15 @@ public class ClubResultsEndpoint {
     private final ClubResultToFactory clubResultToFactory;
     private final CustomChampionshipStandingsService championshipStandingsService;
 
+    private final ApplicationEventPublisher eventPublisher;
+
     @PostMapping(Routes.REFRESH_CLUB_BY_CLUB_ID)
     public void forceRefresh(@PathVariable Long clubId) {
         Club club = clubSyncService.getOrCreate(clubId);
 
         racenetSyncService.fullRefreshClub(club.getReferenceId());
+
+        eventPublisher.publishEvent(new LeaderboardUpdated(clubId));
     }
 
     @GetMapping(Routes.CLUB_EVENT_SUMMARY_BY_CLUB_ID)
