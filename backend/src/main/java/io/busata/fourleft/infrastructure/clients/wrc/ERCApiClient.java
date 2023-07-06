2daff0908c85a841a3a34f8749ea58586a8ed445
busata <driesdesmet@gmail.com>
2023-07-06 20:48:51 +0200

    duplicate feign

--- backend/src/main/java/io/busata/fourleft/infrastructure/clients/wrc/ERCApiClient.java
+++ backend/src/main/java/io/busata/fourleft/infrastructure/clients/wrc/ERCApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="wrc", url = "https://api.fiaerc.com/sdb")
+@FeignClient(name="erc", url = "https://api.fiaerc.com/sdb")
 public interface ERCApiClient {
     @GetMapping("/rallyevent/{rallyEventId}/contelPageId/{contentPageId}}/ticker-entries")
     TickerSummaryTo getTickerSummary(@PathVariable String rallyEventId, @PathVariable String contentPageId);
