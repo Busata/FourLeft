8137506e104fc8123c2ea90cb82650eca5e8afc1
busata <driesdesmet@gmail.com>
2023-07-06 20:19:40 +0200

    add erc

--- backend/src/main/java/io/busata/fourleft/infrastructure/clients/wrc/WRCApiClient.java
+++ backend/src/main/java/io/busata/fourleft/infrastructure/clients/wrc/WRCApiClient.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="wrc", url = "https://api.wrc.com/sdb")
+@FeignClient(name="wrc", url = "api.fiaerc.com/sdb")
 public interface WRCApiClient {
-    @GetMapping("/rallyevent/{rallyEventId}/contelPageId/310819/ticker-entries")
+    @GetMapping("/rallyevent/{rallyEventId}/contelPageId/499746/ticker-entries")
     WRCTickerSummaryTo getTickerSummary(@PathVariable String rallyEventId);
 }
