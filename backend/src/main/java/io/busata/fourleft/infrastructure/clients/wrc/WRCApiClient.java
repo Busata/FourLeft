ae3f0e95a08836e8c17d2ff43a2db61cf34aebb2
busata <driesdesmet@gmail.com>
2023-07-06 20:20:08 +0200

    fix url

--- backend/src/main/java/io/busata/fourleft/infrastructure/clients/wrc/WRCApiClient.java
+++ backend/src/main/java/io/busata/fourleft/infrastructure/clients/wrc/WRCApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="wrc", url = "api.fiaerc.com/sdb")
+@FeignClient(name="wrc", url = "https://api.fiaerc.com/sdb")
 public interface WRCApiClient {
     @GetMapping("/rallyevent/{rallyEventId}/contelPageId/499746/ticker-entries")
     WRCTickerSummaryTo getTickerSummary(@PathVariable String rallyEventId);
