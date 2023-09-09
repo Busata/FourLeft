6974bc283995dfb87ae91e01059731117bfd90d5
Dries De Smet <driesdesmet@gmail.com>
2023-09-09 23:44:07 +0200

    rewrite query

--- backend/src/main/java/io/busata/fourleft/domain/dirtrally2/players/PlayerInfoRepository.java
+++ backend/src/main/java/io/busata/fourleft/domain/dirtrally2/players/PlayerInfoRepository.java
@@ -2,6 +2,7 @@ package io.busata.fourleft.domain.dirtrally2.players;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +10,7 @@ import java.util.UUID;
 
 public interface PlayerInfoRepository extends JpaRepository<PlayerInfo, UUID> {
 
-    @Query(value = "select * from player_info pi where id in (select distinct player_info_id from player_info_racenets where racenets = :name)", nativeQuery = true)
-    Optional<PlayerInfo> findByRacenet(String name);
+    @Query(value = "select * from player_info pi where pi.id in (select distinct player_info_id as id from player_info_racenets pir where pir.racenets = :name)", nativeQuery = true)
+    Optional<PlayerInfo> findByRacenet(@Param("name") String name);
 
 }
