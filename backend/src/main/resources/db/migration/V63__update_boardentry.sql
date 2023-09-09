9eff7cb11e076125464dda700b0a9239736e6f7d
Dries De Smet <driesdesmet@gmail.com>
2023-09-09 23:06:49 +0200

    add index

--- backend/src/main/resources/db/migration/V63__update_boardentry.sql
+++ backend/src/main/resources/db/migration/V63__update_boardentry.sql
@@ -1,1 +1,2 @@
+create index idx_racenets on player_info_racenets(racenets);
 update board_entry be set player_info_id=(select pir.player_info_id from player_info_racenets pir where be.name = pir.racenets);
