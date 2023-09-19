5c65c68141da0d7c70dd76299388376c2656ae07
Dries De Smet <driesdesmet@gmail.com>
2023-09-19 10:59:31 +0200

    add rest controller

--- racenet-authenticator/src/main/java/io/busata/fourleft/racenetauthenticator/endpoints/RacenetAuthenticationEndpoint.java
+++ racenet-authenticator/src/main/java/io/busata/fourleft/racenetauthenticator/endpoints/RacenetAuthenticationEndpoint.java
@@ -5,10 +5,11 @@ import io.busata.fourleft.racenetauthenticator.application.DirtRally2Authenticat
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RequestMapping
+@RestController
 @RequiredArgsConstructor
 public class RacenetAuthenticationEndpoint {
 
