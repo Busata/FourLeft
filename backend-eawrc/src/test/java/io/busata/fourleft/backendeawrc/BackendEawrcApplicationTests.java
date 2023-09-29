7244543c13d555b0381acdf5a6bc73c20d91eb8d
Dries De Smet <driesdesmet@gmail.com>
2023-09-29 12:54:07 +0200

    include backend-eawrc (empty)

---
+++ backend-eawrc/src/test/java/io/busata/fourleft/backendeawrc/BackendEawrcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.busata.fourleft.backendeawrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEawrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
