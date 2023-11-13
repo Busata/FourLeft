bb15f6e889f9a1707e16302ce94ea961377cb268
Dries De Smet <driesdesmet@gmail.com>
2023-11-13 16:32:45 +0100

    update

--- backend-ea-sports-wrc/src/main/java/io/busata/fourleft/backendeasportswrc/endpoints/EventsEndpoint.java
+++ backend-ea-sports-wrc/src/main/java/io/busata/fourleft/backendeasportswrc/endpoints/EventsEndpoint.java
@@ -1,6 +1,8 @@
 package io.busata.fourleft.backendeasportswrc.endpoints;
 
+import io.busata.fourleft.backendeasportswrc.application.discord.configuration.DiscordClubConfigurationService;
 import io.busata.fourleft.backendeasportswrc.application.discord.messages.ClubEventsMessageFactory;
+import io.busata.fourleft.backendeasportswrc.domain.models.DiscordClubConfiguration;
 import io.busata.fourleft.backendeasportswrc.domain.services.championships.ChampionshipService;
 import io.busata.fourleft.backendeasportswrc.domain.services.club.ClubService;
 import lombok.RequiredArgsConstructor;
@@ -13,14 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class EventsEndpoint {
+    private final DiscordClubConfigurationService discordClubConfigurationService;
 
     private final ClubService clubService;
     private final ClubEventsMessageFactory clubEventsMessageFactory;
     private final ChampionshipService championshipService;
 
     @GetMapping("/api_v2/events/{channelId}/summary")
-    String getEventSummary(@PathVariable String clubId) {
-        return clubService.getActiveChampionshipId(clubId)
+    String getEventSummary(@PathVariable Long channelId) {
+        DiscordClubConfiguration discordClubConfiguration = discordClubConfigurationService.findByChannelId(channelId).orElseThrow();
+
+        return clubService.getActiveChampionshipId(discordClubConfiguration.getClubId())
                 .flatMap(championshipService::findChampionship)
                 .map(clubEventsMessageFactory::createEventSummary)
                 .map(MessageEmbed::toData)
