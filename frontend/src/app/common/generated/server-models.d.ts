6f8b904f5feac2687f7369bb4f788cd13838c4b7
Dries De Smet <driesdesmet@gmail.com>
2023-11-13 12:52:55 +0100

    update

--- frontend/src/app/common/generated/server-models.d.ts
+++ frontend/src/app/common/generated/server-models.d.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.35.1025 on 2023-11-13 11:37:55.
+// Generated using typescript-generator version 2.35.1025 on 2023-11-13 12:49:17.
 
 export interface RoutesTo {
 }
