7ea1f09e60dd1ae4730a64e7fed4fcdaaba1215e
Dries De Smet <driesdesmet@gmail.com>
2023-12-18 12:59:10 +0100

    update

--- backend-ea-sports-wrc/src/main/java/io/busata/fourleft/backendeasportswrc/endpoints/ManagementEndpoint.java
+++ backend-ea-sports-wrc/src/main/java/io/busata/fourleft/backendeasportswrc/endpoints/ManagementEndpoint.java
@@ -12,7 +12,7 @@ public class ManagementEndpoint {
 
     @PostConstruct
     public void setup() {
-        this.discordGateway.deleteMessage(1173281341073264745L, 1186273579348791418L);
-        this.discordGateway.deleteMessage(1173281341073264745L, 1186273589301887007L);
+//        this.discordGateway.deleteMessage(1173281341073264745L, 1186273579348791418L);
+//        this.discordGateway.deleteMessage(1173281341073264745L, 1186273589301887007L);
     }
 }
