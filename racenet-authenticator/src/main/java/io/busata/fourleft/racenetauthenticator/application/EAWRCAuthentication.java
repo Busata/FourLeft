c8b800c7088ea936bf18181fb96571907fa4db04
Dries De Smet <driesdesmet@gmail.com>
2024-02-11 07:45:31 +0100

    update

--- racenet-authenticator/src/main/java/io/busata/fourleft/racenetauthenticator/application/EAWRCAuthentication.java
+++ racenet-authenticator/src/main/java/io/busata/fourleft/racenetauthenticator/application/EAWRCAuthentication.java
@@ -49,7 +49,7 @@ public class EAWRCAuthentication {
         System.setProperty("webdriver.chrome.whitelistedIps", "");
 
         WebDriverManager manager = WebDriverManager.chromedriver();
-        manager.driverVersion("119.0.6045.105");
+        //manager.driverVersion("119.0.6045.105");
 
         ChromeOptions capabilities = new ChromeOptions();
 
