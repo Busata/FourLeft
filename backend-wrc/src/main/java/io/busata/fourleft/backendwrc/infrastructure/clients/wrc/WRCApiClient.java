c6b8927b1e500d11c246693c9a29c1418c9e6f47
Dries De Smet <driesdesmet@gmail.com>
2024-05-02 20:26:05 +0200

    enable for erc

--- backend-wrc/src/main/java/io/busata/fourleft/backendwrc/infrastructure/clients/wrc/WRCApiClient.java
+++ backend-wrc/src/main/java/io/busata/fourleft/backendwrc/infrastructure/clients/wrc/WRCApiClient.java
@@ -5,9 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //https://api.wrc.com/content/result/liveUpdates?eventId=449
+//https://api.fiaerc.com/content/result/liveUpdates?eventId=445
 
-@FeignClient(name="wrc", url = "https://api.wrc.com")
+@FeignClient(name="wrc", url = "https://api.fiaerc.com")
 public interface WRCApiClient {
-    @GetMapping("/content/result/liveUpdates?eventId=449")
+    @GetMapping("/content/result/liveUpdates?eventId=445")
     WRCLiveUpdatesTo getLiveUpdates();
 }
