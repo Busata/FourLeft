0c1b9b8e3498e5a1ff3ff7fe5062f6a68ad00a8e
Dries De Smet <driesdesmet@gmail.com>
2024-10-11 16:43:38 +0200

    setup command

--- discord/src/main/java/io/busata/fourleftdiscord/eawrcsports/commands/SetupFinderCommandHandler.java
+++ discord/src/main/java/io/busata/fourleftdiscord/eawrcsports/commands/SetupFinderCommandHandler.java
@@ -16,7 +16,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
