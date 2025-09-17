a55b46295992b1c9f570a79bf36296e0d25bf6c6
Dries De Smet <driesdesmet@gmail.com>
2025-09-17 09:36:04 +0200

    Add more logging for failed result posts

--- backend-ea-sports-wrc/src/main/java/io/busata/fourleft/backendeasportswrc/endpoints/ManagementEndpoint.java
+++ backend-ea-sports-wrc/src/main/java/io/busata/fourleft/backendeasportswrc/endpoints/ManagementEndpoint.java
@@ -1,8 +1,12 @@
 package io.busata.fourleft.backendeasportswrc.endpoints;
 
+import io.busata.fourleft.api.easportswrc.events.ClubEventEnded;
 import io.busata.fourleft.backendeasportswrc.infrastructure.clients.discord.DiscordGateway;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +17,13 @@ public class ManagementEndpoint {
     @PostConstruct
     public void setup() {
        //this.discordGateway.deleteMessage(1173281586184204348L, 1310613982247125032L);
+    }
+
+    private final ApplicationEventPublisher eventPublisher;
+
+
+    @PostMapping("/api_v2/management/test_30129")
+    public void testClubEvent() {
+        this.eventPublisher.publishEvent(new ClubEventEnded("30129"));
     }
 }
